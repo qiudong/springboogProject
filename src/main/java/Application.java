f414f57635fed135f63377f48363fa9e0a483a54
qiudong <qiu_dong@suixingpay.com>
2017-12-19 09:42:32 +0800

    上传springboot项目

---
+++ src/main/java/Application.java
@@ -0,0 +1,67 @@
+
+import java.util.concurrent.LinkedBlockingDeque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@EnableAutoConfiguration
+//@ImportResource(value = {"classpath*:merch_core/core-context.xml"})
+public class Application {
+
+    private static LinkedBlockingDeque<Boolean> RUN = new LinkedBlockingDeque<Boolean>();
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     * @throws Exception the exception
+     */
+    public static void main(String[] args) throws Exception {
+
+        SpringApplication springApplication = new SpringApplication(Application.class);
+
+        springApplication.run(args);
+        //logger.info(print());
+        System.out.println(print());
+
+        while (RUN.take()) {
+        }
+    }
+
+    /**
+     * Stop.
+     */
+    static void stop() {
+        RUN.add(false);
+    }
+
+    private static String print(){
+        StringBuffer sb = new StringBuffer();
+        sb.append("                   _ooOoo_\n");
+        sb.append("                  o8888888o\n");
+        sb.append("                  88\" . \"88\n");
+        sb.append("                  (| ^_^ |)\n");
+        sb.append("                  O\\  =  /O\n");
+        sb.append("               ____/`---'\\____\n");
+        sb.append("             .'  \\\\|     |//  `.\n");
+        sb.append("            /  \\\\|||  :  |||//  \\ \n");
+        sb.append("           /  _||||| -:- |||||-  \\ \n");
+        sb.append("           |   | \\\\\\  -  /// |   |\n");
+        sb.append("           | \\_|  ''\\---/''  |   |\n");
+        sb.append("           \\  .-\\__  `-`  ___/-. /\n");
+        sb.append("         ___`. .'  /--.--\\  `. . __\n");
+        sb.append("      .\"\" '<  `.___\\_<|>_/___.'  >'\"\".\n");
+        sb.append("     | | :  `- \\`.;`\\ _ /`;.`/ - ` : | |\n");
+        sb.append("     \\  \\ `-.   \\_ __\\ /__ _/   .-` /  /\n");
+        sb.append("======`-.____`-.___\\_____/___.-`____.-'======\n");
+        sb.append("                   `=---='\n");
+        sb.append("...................................................\n");
+        return sb.toString();
+    }
+
+
+}
